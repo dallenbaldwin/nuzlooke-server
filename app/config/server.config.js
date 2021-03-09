6b5a56268d6dfabb4e8a3ff090977e88740a4965
Dallen Baldwin <dallen.baldwin@gmail.com>
2021-03-08 18:00:09 -0700

    server is running

---
+++ app/config/server.config.js
@@ -0,0 +1,2 @@
+export const PORT = 3000; // change to 8080 eventually
+export const HOST = 'localhost';
---
+++ server.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import cors from 'cors';
+import { PORT, HOST } from './app/config/server.config.js';
+
+const app = express();
+
+// FIXME: customize cors
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// set routes
+
+app.get('/', (req, res) => {
+   res.status(200).send({
+      message: 'welcome to the nuzlooke-server',
+   });
+});
+
+app.listen(PORT, () => {
+   console.log(`server listening on http://${HOST}:${PORT}/`);
+});
