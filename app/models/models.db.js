aa7956eeb9ee39a855a40160e3e1bd4625971a30
Dallen Baldwin <dallen.baldwin@gmail.com>
2021-03-10 23:51:47 -0700

    added todo

--- app/models/models.db.js
+++ app/models/models.db.js
@@ -3,4 +3,6 @@ import config from '../config/config.db.js';
 
 const client = new AWS.DynamoDB(config);
 
+// FIXME: add logic to define and create tables if they do not exist
+
 export default client;
