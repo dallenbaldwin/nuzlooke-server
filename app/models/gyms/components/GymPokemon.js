0de442dcb5f594f0343faa82277e26c6d8542e86
Dallen Baldwin <dallen.baldwin@gmail.com>
2021-03-12 11:09:27 -0700

    added builder logic and tested

--- app/models/gyms/components/GymPokemon.js
+++ app/models/gyms/components/GymPokemon.js
@@ -9,4 +9,51 @@ export default class GymPokemon {
       this.held_item = heldItem;
       this.moves = moves;
    }
+   static builder() {
+      return this;
+   }
+   static withSpecies(species) {
+      this.species = species;
+      return this;
+   }
+   static withIconUrl(iconUrl) {
+      this.icon_url = iconUrl;
+      return this;
+   }
+   static withSpriteUrl(spriteUrl) {
+      this.sprite_url = spriteUrl;
+      return this;
+   }
+   static withTypes(types) {
+      this.types = types;
+      return this;
+   }
+   static withLevel(level) {
+      this.level = level;
+      return this;
+   }
+   static withAbility(ability) {
+      this.ability = ability;
+      return this;
+   }
+   static withHeldItem(heldItem) {
+      this.held_item = heldItem;
+      return this;
+   }
+   static withMoves(moves) {
+      this.moves = moves;
+      return this;
+   }
+   static build() {
+      return new GymPokemon(
+         this.species,
+         this.icon_url,
+         this.sprite_url,
+         this.types,
+         this.level,
+         this.ability,
+         this.heldItem,
+         this.moves
+      );
+   }
 }
