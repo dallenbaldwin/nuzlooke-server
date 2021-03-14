1f567f9a468c081a911f6785cbf905e472b750b1
Dallen Baldwin <dallen.baldwin@gmail.com>
2021-03-13 23:14:15 -0700

    notes for later

--- app/models/DataClient.js
+++ app/models/DataClient.js
@@ -3,6 +3,8 @@ import config from '../config/db.js';
 
 const DataClient = new AWS.DynamoDB(config);
 
+// Max Object size is 400KB
+
 // FIXME: add logic to define and create tables if they do not exist
 
 export default DataClient;
