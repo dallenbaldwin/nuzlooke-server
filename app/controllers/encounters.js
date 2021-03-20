a57ba572a7f99fad521b6a3330ad28f3444637de
Dallen Baldwin <dallen.baldwin@gmail.com>
2021-03-19 23:10:20 -0600

    eliminated redundant controller

--- app/controllers/encounters.js
+++ app/controllers/encounters.js
@@ -1,6 +1,47 @@
+import { VersionFamily } from '../models/constants/GameVersion.js';
+import Encounter from '../models/encounters/Encounter.js';
+import EncounterPokemon from '../models/encounters/EncounterPokemon.js';
+import EncounterResult from '../models/constants/EncounterResult.js';
+import PokeAPI from '../models/PokeAPI.js';
+import { arrayify } from '../util/UtilMethods.js';
 import { deClassify } from '../util/UtilMethods.js';
-import buildEncounters from './buildEncounters.js';
 
 export function listEncounters(versionFamily) {
    return deClassify(buildEncounters(versionFamily));
 }
+
+function buildEncounters(versionFamily) {
+   switch (versionFamily) {
+      case VersionFamily.LETSGO:
+         // TODO: replace this with actual code
+         const encounters = arrayify(
+            Encounter.builder()
+               .withLabel(`Oak's Lab`)
+               .withResult(EncounterResult.AVAILABLE)
+               .withPokemons(
+                  EncounterPokemon.builder().withSpecies('Pikachu').build(),
+                  EncounterPokemon.builder().withSpecies('Eevee').build()
+               )
+               .build(),
+            Encounter.builder()
+               .withLabel('Route 1')
+               .withResult(EncounterResult.AVAILABLE)
+               .withPokemons(
+                  EncounterPokemon.builder().withSpecies('Pidgey').build(),
+                  EncounterPokemon.builder().withSpecies('Rattata').build()
+               )
+               .build()
+         );
+         return encounters;
+      default:
+         return [];
+   }
+}
+
+// TODO: Figure out how this is going to work
+/*
+- get the region from the version family
+- 
+
+
+*/
