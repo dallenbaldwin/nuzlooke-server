0b748798508b6974d63da0c73ade7768f26bc764
Dallen Baldwin <dallen.baldwin@gmail.com>
2021-03-25 22:42:52 -0600

    added axios

--- app/models/PokeAPI.js
+++ app/models/PokeAPI.js
@@ -1,15 +1,7 @@
-import Pokedex from 'pokedex-promise-v2';
-
-// how long to keep the cache alive. keeps the process alive, which is fine for when the server is going, but for quick testing, set to a low value
-const cacheSeconds = 1;
-// request timeout seconds
-const timeoutSeconds = 20;
-
-const options = {
-   cacheLimit: cacheSeconds * 1000,
-   timeout: timeoutSeconds * 1000,
-};
-
-const PokeAPI = new Pokedex(options);
-
-export default PokeAPI;
+import axios from 'axios';
+export default class PokeAPI {
+   constructor() {
+      this.client = axios.create({});
+      this.baseURL = 'https://pokeapi.co/api/v2';
+   }
+}
