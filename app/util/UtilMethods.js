f88e468314085a0bd2ffb2c27c589fd720f14b88
Dallen Baldwin <dallen.baldwin@gmail.com>
2021-04-24 15:07:56 -0600

    cleaned up arrayify

--- app/util/UtilMethods.js
+++ app/util/UtilMethods.js
@@ -6,11 +6,7 @@ export const fromAWSItem = object => AWS.DynamoDB.Converter.unmarshall(object);
 
 export const isUndefined = value => value === undefined || value === null;
 
-export const arrayify = (...values) => {
-   const a = [];
-   values.forEach(value => a.push(value));
-   return a.flatMap(i => i); // force it to accept an array of values or a list of values
-};
+export const arrayify = (...values) => Array.from([...values]).flatMap(i => i); // force it to accept an array of values or a list of values
 
 export const deClassify = classObject => JSON.parse(JSON.stringify(classObject));
 
