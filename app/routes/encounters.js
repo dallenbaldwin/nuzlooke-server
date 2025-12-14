9780918c64086cb79841c453a654d48c8d3e196c
Dallen Baldwin <dallen.baldwin@gmail.com>
2025-12-14 01:02:46 -0700

    stuff is borked

--- app/routes/encounters.js
+++ app/routes/encounters.js
@@ -1,6 +1,19 @@
 import { verifyToken } from '../controllers/auth.js';
 import { getEncounters } from '../controllers/encounters/encounters.js';
+import APIResponse from '../models/APIResponse.js';
 
 export default app => {
-   app.get('/api/encounters/:version', verifyToken, getEncounters);
+   app.get(
+      '/api/encounters/:version',
+      verifyToken,
+      true
+         ? (_, res) => {
+              res.status(405).send(
+                 APIResponse.withError(
+                    'encounters endpoint is broken when watch mode is enabled'
+                 )
+              );
+           }
+         : getEncounters
+   );
 };
